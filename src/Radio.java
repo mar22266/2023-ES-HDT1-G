e094c6e451021f9f46d540bfd00a8f6096ec54ea
Andre <andremt2003@gmail.com>
2023-01-15 20:04:53 -0600

    3

--- src/Radio.java
+++ src/Radio.java
@@ -18,76 +18,113 @@ public class Radio implements IRadio{
 
     @Override
     public void on() {
+        this.on = true;
 
     }
 
     @Override
     public void off() {
+        this.on = false;
 
     }
 
     @Override
     public boolean isOn() {
-        return false;
+        return on;
     }
 
     @Override
     public void setFrequence(String freq) throws Exception {
+        try{
+            if(freq.equals("AM") || freq.equals("FM")){
+                this.frequence = freq;
+            }else{
+                throw new Exception("Error");
+            }}
+            catch(Exception e){
+                System.out.println("ERROR");
+            }
 
     }
 
     @Override
     public String getFrequence() {
-        return null;
+        return frequence;
     }
 
     @Override
     public void Forward() {
-
+        if(this.frequence.equals("AM")){
+            if(this.AMActualStation == 1610){
+                this.AMActualStation = 530;
+            }else{
+                this.AMActualStation += 10;
+            }
+        }else{
+            if(this.FMActualStation == 107.9){
+                this.FMActualStation = 87.9;
+            }else{
+                this.FMActualStation += 0.2;
+            }
+        }
     }
 
     @Override
     public void Backward() {
-
+        if(this.frequence.equals("AM")){
+            if(this.AMActualStation == 530){
+                this.AMActualStation = 1610;
+            }else{
+                this.AMActualStation -= 10;
+            }
+        }else{
+            if(this.FMActualStation == 87.9){
+                this.FMActualStation = 107.9;
+            }else{
+                this.FMActualStation -= 0.2;
+            }
+        }
     }
 
     @Override
     public double getFMActualStation() {
-        return 0;
+        return FMActualStation;
     }
 
     @Override
     public int getAMActualStation() {
-        return 0;
+        return AMActualStation;
     }
 
     @Override
     public void setFMActualStation(double actualStation) {
+        this.FMActualStation = actualStation;
+
 
     }
 
     @Override
     public void setAMActualStation(int actualStation) {
-
+        this.AMActualStation = actualStation;
     }
 
     @Override
     public void saveFMStation(double actualStation, int slot) {
-
+        this.FMSlot[slot] = actualStation;
     }
 
     @Override
     public void saveAMStation(int actualStation, int slot) {
-
+        this.AMSlot[slot] = actualStation;
     }
 
     @Override
     public double getFMSlot(int slot) {
-        return 0;
+        return FMSlot[slot];
     }
 
     @Override
     public int getAMSlot(int slot) {
-        return 0;
+        return AMSlot[slot];
     }
 }
