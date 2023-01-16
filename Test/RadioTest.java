d2d82a42c3cf5de60ff8f967eaa46fad0976bcf3
Andre <andremt2003@gmail.com>
2023-01-15 18:21:19 -0600

    test

---
+++ Test/RadioTest.java
@@ -0,0 +1,66 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    void on() {
+    }
+
+    @Test
+    void off() {
+    }
+
+    @Test
+    void isOn() {
+    }
+
+    @Test
+    void setFrequence() {
+    }
+
+    @Test
+    void getFrequence() {
+    }
+
+    @Test
+    void forward() {
+    }
+
+    @Test
+    void backward() {
+    }
+
+    @Test
+    void getFMActualStation() {
+    }
+
+    @Test
+    void getAMActualStation() {
+    }
+
+    @Test
+    void setFMActualStation() {
+    }
+
+    @Test
+    void setAMActualStation() {
+    }
+
+    @Test
+    void saveFMStation() {
+    }
+
+    @Test
+    void saveAMStation() {
+    }
+
+    @Test
+    void getFMSlot() {
+    }
+
+    @Test
+    void getAMSlot() {
+    }
+}
